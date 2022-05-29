fb277396e08971774e62a303c93a62a44338786c
duttaarun <duttaarun2015@gmail.com>
2022-05-29 20:32:11 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,99 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
 
   const wave = () => {
-    
+
   }
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    /*
+    * First make sure we have access to window.ethereum
+    */
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+   /**
+  * Implement your connectWallet method here
+  */
+    const connectWallet = async () => {
+      try {
+        const { ethereum } = window;
   
+        if (!ethereum) {
+          alert("Get MetaMask!");
+          return;
+        }
+
+        await ethereum.request({
+          method: 'wallet_requestPermissions',
+          params: [{ eth_accounts: {} }]
+        });
+  
+        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  
+        console.log("Connected", accounts[0]);
+        setCurrentAccount(accounts[0]);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="mainContainer">
 
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          👋 Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
